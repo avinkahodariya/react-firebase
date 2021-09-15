0cdb7c5d5fa682afeb4ccfdb9c05c2886a709024
avinkahodariya <avinkahodariya@gmail.com>
2021-09-16 00:21:49 +0530

    booking firebae data save

--- src/main page/Booking.js
+++ src/main page/Booking.js
@@ -1,35 +1,97 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../config";
-
+import TextField from "@material-ui/core/TextField";
+import styles from "../css.module.css";
+import Button from "@material-ui/core/Button";
+import { getDatabase, ref, onValue } from "firebase/database";
 const Booking = () => {
-  const [currency, set_currency] = useState([]);
   const [countries, set_countries] = useState([]);
-  useEffect(async () => {
-    // useEffect(() => {
-    //     let data = await db.collection("skyscaner").add({
-    //       });
-    // }, [])
-    // const set = async () => {
-    //   await db.collection("skyscanner").add({
-    //     countries: countries,
-    //   });
-    //   await db.collection("skyscanner").add({
-    //     currency: currency,
-    //   });
-    // };
-    // set();
+  const [Input, set_Input] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+
+  const fetchBlogs = async () => {
+    const response = db.collection("countries");
+    const data = await response.get();
+    console.log("data", data);
+    data.docs.forEach((item) => {
+      console.log("item.data()data", item.data().countries.Countries);
+
+      setBlogs(item.data().countries.Countries);
+    });
+    console.log(blogs, countries);
+  };
+  useEffect(() => {
+    fetchBlogs();
   }, []);
+  // console.log("dcrcrata", blogs);
 
-  //   const set = async () => {
-  //     await db.collection("skyscanner").add({
-  //       countries: countries,
-  //     });
-  //     await db.collection("skyscanner").add({
-  //       currency: currency,
-  //     });
-  //   };
+  const handleSubmit = () => {
+    // fetchBlogs();
+    console.log("blobh", blogs, Input);
 
-  return <div></div>;
+    let result = blogs.filter((a) => {
+      console.log("as", a);
+      if (a.Name.toLowerCase().includes(Input)) {
+        return a.Name;
+      }
+    });
+
+    console.log("res b", result);
+    set_countries(result);
+    // let results = blogs.filter((x) => x.includes(Input));
+    // console.log("as res", results);
+  };
+  // console.log("countries", countries);
+  console.log("dcrcrata", typeof blogs);
+
+  return (
+    <>
+      <div className={styles.Search}>
+        {" "}
+        <TextField
+          required
+          id="standard-required"
+          label="Input"
+          // type="email"
+          value={Input}
+          variant="outlined"
+          className={styles.MuiTextField}
+          onChange={(e) => {
+            console.log("df", e.target.value);
+            set_Input(e.target.value);
+          }}
+        />{" "}
+        <TextField
+          required
+          id="standard-required"
+          label="Input"
+          // type="email"
+          // value={Input}
+          variant="outlined"
+          className={styles.MuiTextField}
+          // onChange={(e) => {
+          //   console.log("df", e.target.value);
+          //   set_Input(e.target.value);
+          // }}
+        />{" "}
+        <Button
+          variant="contained"
+          color="primary"
+          // className={}
+          onClick={handleSubmit}
+        >
+          Search{" "}
+        </Button>
+      </div>
+      {countries && (
+        <>
+          {countries.map((place) => {
+            return <li>{place.Name}</li>;
+          })}
+        </>
+      )}
+      <div></div>
+    </>
+  );
 };
-
 export default Booking;
